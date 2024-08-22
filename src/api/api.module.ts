b8ffc44ad2c9ea1eddc21948d01673f39c3f5e1f
maycondbraga <maycondbraga.s@gmail.com>
2024-08-22 01:07:22 -0300

    feat: start api module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,4 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
 
-@Module({})
+@Module({
+  imports: [HttpModule],
+  controllers: [ApiController],
+  providers: [ApiService],
+})
 export class ApiModule {}
